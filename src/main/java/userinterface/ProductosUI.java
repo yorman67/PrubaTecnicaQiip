6e714c2f39f1c2c6d2bb09048a02df471ae2a883
Yorman <yorman.p.martinez@gmail.com>
2021-09-15 18:19:05 -0500

    creacion de rama

--- src/main/java/userinterface/ProductosUI.java
+++ src/main/java/userinterface/ProductosUI.java
@@ -6,6 +6,5 @@ import net.serenitybdd.screenplay.targets.Target;
 public class ProductosUI {
     public static final Target LBL_NOMBRE_PRODUCTO = Target.the("Titulo del producto").locatedBy("//android.widget.TextView[@text='{0}']");
     public static final Target BTN_AGREGAR_ARTICULO = Target.the("Boton para agregar un articulo").located(MobileBy.AccessibilityId("test-ADD TO CART"));
-    public static final Target BTN_BACK_TO_HOME= Target.the("Button Back to the Home").located(MobileBy.AccessibilityId("test-BACK TO PRODUCTS"));
     public static final Target BTN_CARRITO= Target.the("Boton para ir al carrito").located(MobileBy.AccessibilityId("test-Cart"));
 }
--- src/test/java/stepdefinitions/RealizarCompraStepDefinition.java
+++ src/test/java/stepdefinitions/RealizarCompraStepDefinition.java
@@ -3,7 +3,6 @@ package stepdefinitions;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
-import net.serenitybdd.screenplay.actors.OnStage;
 
 import java.util.List;
 import java.util.Map;
@@ -11,7 +10,7 @@ import java.util.Map;
 public class RealizarCompraStepDefinition {
 
     @Dado("que el usuario los productos")
-    public void queElUsuarioLosProductos(List<Map<String, String>> products) {
+    public void queElUsuarioLosProductos(List<Map<String, String>> productos) {
 
     }
 
