0907ab04e847aa19301823b81d652678f78a3350
Yorman <yorman.p.martinez@gmail.com>
2021-09-16 11:14:51 -0500

    modificacion de variables de kobitton

--- src/main/java/util/Enums/EnumCapabilitiesKobitton.java
+++ src/main/java/util/Enums/EnumCapabilitiesKobitton.java
@@ -15,7 +15,7 @@ public enum EnumCapabilitiesKobitton {
     VERSION_KOBITTON("10"),
     PLATFORM_NAME_KOBITTON("Android"),
     NOMBRE_DISPOSITIVO_KOBITTON("Galaxy A10"),
-    URL_KOBITTON("https://yorman67:0c35c89e-e44e-4d75-93c3-575fd550245d@api.kobiton.com/wd/hub");
+    URL_KOBITTON("");
 
     private String texto;
 
